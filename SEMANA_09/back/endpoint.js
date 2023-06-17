762d29162173a010af3cef71be21f8f3e606275a
sophia.nobrega@sou.inteli.edu.br <sophia.nobrega@sou.inteli.edu.br>
2023-06-17 13:42:27 -0300

    sim

--- SEMANA_09/back/endpoint.js
+++ SEMANA_09/back/endpoint.js
@@ -13,34 +13,39 @@ if (err){
 app.use(express.static("../front/"));
 app.use(express.json());
 
-//mantem o servidor on fire
+// nmantem o servidor on fire
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
 	});
 
-//chamada post do nome do usuario e geração do id dele e buscar player
-app.post('/inserir-nome', (req, res) => { 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-	const insert = 'INSERT INTO player (nome) VALUES (?) ';
-	db.run(insert, [req.body.nome], (err) => {
+// chamada post do nome do usuario e geração do id dele e buscar playe
+app.post('/inserir-nome', (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	const insert = 'INSERT INTO player (nome) VALUES (?)';
+	db.run(insert, [req.body.nome], function(err) {
 		if (err) {
 			console.error(err.message);
 			res.status(500).json({ error: 'Vish, não rolou não' });
 		} else {
-		res.status(201).json({ message: 'Dentro' });
+			const playerId = this.lastID; // ramazena o id do jogador inserido
+			res.status(201).json({ message: 'Dentro', playerId: playerId });
 		}
 	});
 });
-//chamada get do id do jogador adversario 
+	
+// chamada get do id do jogador adversario 
 app.get('/players', (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-    var id = `SELECT id FROM player WHERE nome = ${insert}`;
-	const get3 = `SELECT id FROM player WHERE id != ${id}`;
-	db.all(get3, [], (err, rows) => { 
+	const playerId = req.query.playerId; // obtém o id do jogador atual
+	const getOpponents = 'SELECT id FROM player WHERE id != ?';
+	db.all(getOpponents, [playerId], (err, rows) => {
 		if (err) {
 			console.error(err.message);
+			res.status(500).json({ error: 'Erro ao obter jogadores' });
+		} else {
+			res.json(rows);
 		}
-		res.json(rows);
 	});
 });
+	
 
